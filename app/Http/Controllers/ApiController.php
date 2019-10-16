af9e9b2b89e4e7d536472ad9f8f1b6fc1c12f5bf
Sambrana Ivan <sambranaivan@gmail.com>
2019-10-16 18:25:28 -0300

    API login

---
+++ app/Http/Controllers/ApiController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\user;
+class ApiController extends Controller
+{
+    //
+    public function login(request $formdata)
+    {
+        $lu =  $formdata->get('lu');
+        $dni =  $formdata->get('dni');
+
+        $alumno = user::where(['lu'=>$lu,'dni'=>$dni])->first();
+
+        if($alumno)
+        {
+            return response()->json($alumno,200);
+        }
+        else
+        {
+            return response("Alumno no existe",400);
+        }
+
+    }
+}
