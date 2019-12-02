02f715d31eeef0bed33ee2fa8dabed787f75ab75
Sambrana Ivan <sambranaivan@gmail.com>
2019-12-01 21:06:27 -0300

    corrigo codigo de error enlogin

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -23,7 +23,7 @@ class ApiController extends Controller
         else
         {
 
-            return  response()->json('{data:"error"}', 200);
+            return  response()->json('{data:"error"}', 400);
         }
 
     }
@@ -58,8 +58,7 @@ class ApiController extends Controller
                 }
                 else
                 {
-                    return response("Fuera de Horario",400);
-
+                    return response("Fuera de Horario",200);
                 }
             }
             else
